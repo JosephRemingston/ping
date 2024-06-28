8fff88e5097877c93b5723873c4b960cb5c0770c
ljremi <ljremi@gmail.com>
2024-06-28 17:32:36 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+class WebSocketService {
+  final WebSocketChannel channel;
+
+  WebSocketService(String url) : channel = WebSocketChannel.connect(Uri.parse(url));
+
+  Stream get stream => channel.stream;
+
+  void sendMessage(String message) {
+    channel.sink.add(message);
+  }
+
+  void dispose() {
+    channel.sink.close();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+
+class ChatMessage {
+  final String message;
+  final DateTime timestamp;
+
+  ChatMessage(this.message, this.timestamp);
+
+  Map<String, dynamic> toJson() => {
+        'message': message,
+        'timestamp': timestamp.toIso8601String(),
+      };
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) {
+    return ChatMessage(
+      json['message'],
+      DateTime.parse(json['timestamp']),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ChatStorage {
+  Future<String> _localPath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Future<File> _localFile() async {
+    final path = await _localPath();
+    return File('$path/chat_messages.json');
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  Future<List<ChatMessage>> readMessages() async {
+    try {
+      final file = await _localFile();
+      final contents = await file.readAsString();
+      final List<dynamic> jsonList = json.decode(contents);
+      return jsonList.map((json) => ChatMessage.fromJson(json)).toList();
+    } catch (e) {
+      return [];
+    }
+  }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Future<void> writeMessages(List<ChatMessage> messages) async {
+    final file = await _localFile();
+    final List<Map<String, dynamic>> jsonList =
+        messages.map((message) => message.toJson()).toList();
+    await file.writeAsString(json.encode(jsonList));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void main() => runApp(MyApp());
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final WebSocketService _webSocketService =
+      WebSocketService('ws://your-websocket-url');
+  final ChatStorage _chatStorage = ChatStorage();
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+    _webSocketService.stream.listen((message) {
+      final chatMessage = ChatMessage(message, DateTime.now());
+      setState(() {
+        _messages.add(chatMessage);
+      });
+      _saveMessages();
     });
+  }
+
+  Future<void> _loadMessages() async {
+    final messages = await _chatStorage.readMessages();
+    setState(() {
+      _messages.addAll(messages);
+    });
+  }
+
+  Future<void> _saveMessages() async {
+    await _chatStorage.writeMessages(_messages);
+  }
+
+  void _sendMessage() {
+    final message = _controller.text;
+    if (message.isNotEmpty) {
+      _webSocketService.sendMessage(message);
+      final chatMessage = ChatMessage(message, DateTime.now());
+      setState(() {
+        _messages.add(chatMessage);
+      });
+      _saveMessages();
+      _controller.clear();
+    }
+  }
+
+  @override
+  void dispose() {
+    _webSocketService.dispose();
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Chat App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Text(message.message),
+                  subtitle: Text(message.timestamp.toIso8601String()),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Send a message',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
